1ec79d884190f2e8825ce9578202d35c503248c3
mostafa <eng.mostafa.attaey@gmail.com>
2022-02-26 23:38:49 +0430

    start,stop and reset functionalities added

--- script.js
+++ script.js
@@ -1,23 +1,71 @@
-var msec = document.getElementById("msec");
-var sec = document.getElementById("sec");
+// mili seconds, seconds and minutes variables.
+var appendMsec = document.getElementById("msec");
+var appendSec = document.getElementById("sec");
+var appendMin = document.getElementById("min");
 
-var start = document.getElementById("start");
-var reset = document.getElementById("reset");
-var stop = document.getElementById("stop");
-var timer = 00;
+//buttons variables
+var btnStart = document.getElementById("button-start");
+var btnReset = document.getElementById("button-reset");
+var btnStop = document.getElementById("button-stop");
 
+// helper values.
+var tens = 00;
+var seconds = 00;
+var minutes = 00;
+
+// interval for looping a function
 var interval;
-start.onclick = function milseconds(){
 
+btnStart.onclick = function (){
+    clearInterval(interval);
     interval = setInterval(startTimer,10);
 }
 
-function startTimer(){
-    timer ++;
-    msec.innerHTML = timer;
-}
-
-
-stop.onclick = function Stop(){
+btnStop.onclick = function (){
     clearInterval(interval);
 }
+
+btnReset.onclick = function res(){
+    clearInterval(interval);
+    tens = "00";
+    seconds = "00";
+    minutes = "00";
+
+    appendMsec.innerHTML = tens;
+    appendSec.innerHTML = seconds;
+    appendMin.innerHTML = minutes;
+}
+// this function will work the main functionality of our stopwatch app
+function startTimer(){
+    
+    tens ++;
+    if(tens <= 9){
+        appendMsec.innerHTML = "0" + tens;
+    }
+    if(tens > 9){
+        appendMsec.innerHTML = tens;
+    }
+    if(tens > 99){
+        seconds ++;
+
+        appendSec.innerHTML = "0" + seconds;
+        tens = 0;
+        appendMsec.innerHTML = "0" + tens;
+    }
+    if(seconds > 9){
+        appendSec.innerHTML = seconds;
+    }
+
+    if(seconds >= 59 ){
+        minutes ++;
+       
+        appendMin.innerHTML = "0"+minutes;
+        seconds = 0;
+        appendSec.innerHTML = "0" + seconds;
+
+    }
+    if(minutes > 9){
+        appendMin.innerHTML = minutes;
+    }
+}
+
